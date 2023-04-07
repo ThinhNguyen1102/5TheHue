ba51f9909e078b49ebabb26555cb4550d037f868
ThinhNguyen1102 <thinhtheblues1102@gmail.com>
2023-04-07 23:35:50 +0700

    fix: remote images

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const datas = [
   {
     id: 1,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_1_bun_bo.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_01.png",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883943/5thehue/images/am_thuc_1_bun_bo_oi9h97.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_01_kiqlaq.png",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -15,9 +18,12 @@ const datas = [
   {
     id: 2,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_2.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_02.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883944/5thehue/images/am_thuc_2_dojrsw.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_02_oihuq7.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -26,9 +32,12 @@ const datas = [
   {
     id: 3,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_3.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_03.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883944/5thehue/images/am_thuc_3_hucjmw.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_03_szemiy.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -37,9 +46,12 @@ const datas = [
   {
     id: 4,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_4_bot_loc_heo_quay.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_04.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883944/5thehue/images/am_thuc_4_bot_loc_heo_quay_t5qfv1.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_04_dcrbs1.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -48,9 +60,12 @@ const datas = [
   {
     id: 5,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_5_cafe_muoi.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_05.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883944/5thehue/images/am_thuc_5_cafe_muoi_zxcla8.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_05_slkp2c.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -59,9 +74,12 @@ const datas = [
   {
     id: 6,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_6_tra_sen_tinh_tam.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_06.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883945/5thehue/images/am_thuc_6_tra_sen_tinh_tam_dfqoiv.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_06_vpkckz.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -70,9 +88,12 @@ const datas = [
   {
     id: 7,
     question: "How do you feel?",
-    mainImageUrl: "./images/am_thuc_7_banh_canh.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_07.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883945/5thehue/images/am_thuc_7_banh_canh_twixhj.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_07_j7uphl.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -81,9 +102,12 @@ const datas = [
   {
     id: 8,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_1.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_08.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883945/5thehue/images/con_nguoi_1_wnlcjk.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_08_divcxz.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -92,9 +116,12 @@ const datas = [
   {
     id: 9,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_2.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_09.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883946/5thehue/images/con_nguoi_2_cbeh3h.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_09_cwxhpk.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -103,9 +130,12 @@ const datas = [
   {
     id: 10,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_3.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_10.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883945/5thehue/images/con_nguoi_3_dmoyiq.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883986/5thehue/images/5TheHue/5TheHue_10_uc5umk.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -114,9 +144,12 @@ const datas = [
   {
     id: 11,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_4.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_11.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883946/5thehue/images/con_nguoi_4_bv9cs1.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_11_ojcj4l.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -125,9 +158,12 @@ const datas = [
   {
     id: 12,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_5.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_12.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/con_nguoi_5_ewqoim.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_12_lxhziq.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -136,9 +172,12 @@ const datas = [
   {
     id: 13,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_6.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_13.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/con_nguoi_6_gdvo2r.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_13_mdntyv.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -147,9 +186,12 @@ const datas = [
   {
     id: 14,
     question: "How do you feel?",
-    mainImageUrl: "./images/con_nguoi_7.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_14.gif",
+    mainImageUrl:
+      "./images/https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/con_nguoi_7_ko4p1r.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_14_gq2ryl.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -158,9 +200,12 @@ const datas = [
   {
     id: 15,
     question: "How do you feel?",
-    mainImageUrl: "./images/fun_1.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_15.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/fun_1_frf6yo.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_15_xhqkru.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -169,9 +214,12 @@ const datas = [
   {
     id: 16,
     question: "How do you feel?",
-    mainImageUrl: "./images/fun_2.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_16.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883948/5thehue/images/fun_2_axfjrx.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_16_nxiais.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -180,9 +228,12 @@ const datas = [
   {
     id: 17,
     question: "How do you feel?",
-    mainImageUrl: "./images/fun_4.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_17.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883948/5thehue/images/fun_4_xtjx6x.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_17_hjenml.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -191,9 +242,12 @@ const datas = [
   {
     id: 18,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_7_chua_thien_mu.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_18.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883950/5thehue/images/Kien_truc_7_chua_thien_mu_buwpke.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883987/5thehue/images/5TheHue/5TheHue_18_dh1he7.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -202,9 +256,12 @@ const datas = [
   {
     id: 19,
     question: "How do you feel?",
-    mainImageUrl: "./images/fun.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_19.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883949/5thehue/images/fun_lwessz.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_19_bksdpv.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -213,9 +270,12 @@ const datas = [
   {
     id: 20,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_1_dai_noi_hue.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_20.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883948/5thehue/images/Kien_truc_1_dai_noi_hue_dfczvz.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_20_rty36e.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -224,9 +284,12 @@ const datas = [
   {
     id: 21,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_2_lang_khai_dinh.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_21.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883949/5thehue/images/Kien_truc_2_lang_khai_dinh_oiuzcw.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_21_xfisut.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -235,9 +298,12 @@ const datas = [
   {
     id: 22,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_3_lang_huong.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_22.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883949/5thehue/images/Kien_truc_3_lang_huong_yizj23.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_22_iohmf2.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -246,9 +312,12 @@ const datas = [
   {
     id: 23,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_4_cau_truong_tien.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_23.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883949/5thehue/images/Kien_truc_4_cau_truong_tien_cqp8f8.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_23_zxwitt.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -257,9 +326,12 @@ const datas = [
   {
     id: 24,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_5_quoc_hoc_hue.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_24.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883949/5thehue/images/Kien_truc_5_quoc_hoc_hue_bsieyq.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883988/5thehue/images/5TheHue/5TheHue_24_i7vdjh.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
@@ -268,9 +340,12 @@ const datas = [
   {
     id: 25,
     question: "How do you feel?",
-    mainImageUrl: "./images/Kien_truc_6_dan_nam_giao.png",
-    defaultImageUrl: "./images/default.jpg",
-    resultImage: "./images/5TheHue/5TheHue_25.gif",
+    mainImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883950/5thehue/images/Kien_truc_6_dan_nam_giao_jxnpxd.png",
+    defaultImageUrl:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883947/5thehue/images/default_cdgr5b.jpg",
+    resultImage:
+      "https://res.cloudinary.com/dkdwgdq4i/image/upload/v1680883989/5thehue/images/5TheHue/5TheHue_25_s3joei.gif",
     isDone: false,
     isCorrect: false,
     status: 1,
